260f5c17db87ef30bfbb2ee0b0770c87bafa77ee
Alex MacArthur <alex.macarthur@daveramsey.com>
2019-07-26 11:02:18 -0500

    Load endpoint and analytics ID from environment variables.

--- README.md
+++ README.md
@@ -10,14 +10,16 @@ In the root of the project, run `npm install`.
 
 ## Usage
 
-### Set Your Constants
+### Set Your Environment Variables
 
-Set your specific constants in the `vue.config.js` file.
+Various important values are loaded into the application via Node environment variables, which you'll need to define. Locally, run `cp .env.sample .env.local` to create a local file for defining the following:
 
 - `REST_ENDPOINT` - The WordPress REST API endpoint from which data will be pulled. Leave off the trailing slash. Example: `https://blah-blah-blah.com/wp-json/wp/v2`
 - `POSTS_PER_PAGE` - The default number of posts per page that will be displayed.
 - `GA_TRACKING_ID` - A Google Analytics tracking ID.
 - `REQUEST_CACHE_MAX` - The maximum number of AJAX requests that will be cached in memory.
+
+When deploying this on your own, you'll need to have these values set through a `.env` file you ship yourself, or if you're using something like Netlify, you can define them in your dashboard.
 
 ### Spin Up Locally
 
--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,25 @@
-const webpack = require('webpack');
-const path = require('path');
-const isProd = process.env.NODE_ENV === 'production';
+const webpack = require("webpack");
+const path = require("path");
+const isProd = process.env.NODE_ENV === "production";
 
 const CONSTANTS = {
-  REST_ENDPOINT: JSON.stringify('https://wptavern.com/wp-json/wp/v2'),
-  POSTS_PER_PAGE: JSON.stringify(9),
-  GA_TRACKING_ID: JSON.stringify(isProd ? 'UA-111438908-1' : 'XXX'),
-  REQUEST_CACHE_MAX: JSON.stringify(150)
+  POSTS_PER_PAGE: JSON.stringify(process.env.POSTS_PER_PAGE),
+  REQUEST_CACHE_MAX: JSON.stringify(process.env.REQUEST_CACHE_MAX),
+  REST_ENDPOINT: JSON.stringify(process.env.REST_ENDPOINT),
+  GA_TRACKING_ID: JSON.stringify(process.env.GA_TRACKING_ID)
 };
 
 module.exports = {
   pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
+    "style-resources-loader": {
+      preProcessor: "scss",
       patterns: [
-        path.resolve(__dirname, './src/assets/scss/_variables.scss'),
-        path.resolve(__dirname, './src/assets/scss/_mixins.scss')
+        path.resolve(__dirname, "./src/assets/scss/_variables.scss"),
+        path.resolve(__dirname, "./src/assets/scss/_mixins.scss")
       ]
     }
   },
   configureWebpack: {
-    plugins: [
-      new webpack.DefinePlugin(CONSTANTS),
-    ]
+    plugins: [new webpack.DefinePlugin(CONSTANTS)]
   }
-}
+};
